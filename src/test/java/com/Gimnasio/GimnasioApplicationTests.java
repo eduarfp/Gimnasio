ca8c15877f5edf687d291f81544436b154b7e99f
Eduardo <Eduardo@LAPTOP>
2022-06-09 13:52:34 -0600

    Creación de Proyecto Final Web y Patrones

---
+++ src/test/java/com/Gimnasio/GimnasioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gimnasio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimnasioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
